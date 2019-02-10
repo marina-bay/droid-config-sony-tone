9aa25b1c9ade1b03a4451175477ed82c4b327909
ix5 <ix5@users.noreply.github.com>
2019-02-10 17:07:46 +0100

    Get rid of all slot and vendor partition references

--- sparse/usr/bin/droid/droid-bootctl.sh
+++ sparse/usr/bin/droid/droid-bootctl.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
+# Disabled for now on tone
 
 # Check currentl used boot slot
-SLOT="$(/usr/libexec/droid-hybris/system/bin/bootctl get-current-slot)"
+#SLOT="$(/usr/libexec/droid-hybris/system/bin/bootctl get-current-slot)"
 
 # Check if current slot is marked successful
-/usr/libexec/droid-hybris/system/bin/bootctl is-slot-marked-successful $SLOT 2> /dev/null
+#/usr/libexec/droid-hybris/system/bin/bootctl is-slot-marked-successful $SLOT 2> /dev/null
 
-if [ $? -ne 0 ]
-then
-    echo "Marking boot as successful"
-    /usr/libexec/droid-hybris/system/bin/bootctl mark-boot-successful 2> /dev/null
-fi
-
+#if [ $? -ne 0 ]
+#then
+#    echo "Marking boot as successful"
+#    /usr/libexec/droid-hybris/system/bin/bootctl mark-boot-successful 2> /dev/null
+#fi
--- sparse/var/lib/platform-updates/factory-reset-vendor.sh
+++ sparse/var/lib/platform-updates/factory-reset-vendor.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 # Copy backup partition (vendor_a) to vendor_b
-dd if=/dev/mmcblk0p70 of=/dev/mmcblk0p71 bs=1M
+# Disabled for now on tone
+#dd if=/dev/mmcblk0p70 of=/dev/mmcblk0p71 bs=1M
 
